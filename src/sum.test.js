ca8f8c78a8edf45a0306e0e324176ad7b169b998
kamil-homer <kamilhomer@gmail.com>
2024-12-22 10:45:02 +0100

    pierwszy test

--- src/sum.test.js
+++ src/sum.test.js
@@ -12,12 +12,3 @@ test("should return 5 for 2 and 3", () => {
   // Assert
   expect(result).toBe(5);
 });
-
-test("should return 5 for 2 and 3 as text", () => {
-  const num1 = "2";
-  const num2 = "3";
-
-  const result = sum(num1, num2);
-
-  expect(result).toBe(5);
-});
