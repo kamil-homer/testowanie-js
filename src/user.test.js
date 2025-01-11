0733b6fbbd547d1de35975fe7b82cbf13e72722a
kamil-homer <kamilhomer@gmail.com>
2025-01-11 16:41:44 +0100

    updated files

--- src/user.test.js
+++ src/user.test.js
@@ -2,9 +2,13 @@ import { expect, test } from "vitest";
 import { getUserById } from "./user";
 
 test("should throw an error when ID is not provided", () => {
-  expect(() => getUserById()).toThrow("ID is required");
+  const resultFn = () => getUserById();
+
+  expect(resultFn).toThrow("ID is required");
 });
 
 test("should return user details", () => {
-  expect(getUserById("123")).toEqual({ name: "John" });
+  const user = getUserById("123");
+
+  expect(user).toEqual({ name: "John" });
 });
