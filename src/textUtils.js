f1d7586e4067a4d124cc76621167cbc69fa035f2
kamil-homer <kamilhomer@gmail.com>
2025-01-11 15:51:06 +0100

    updated tests and file

--- src/textUtils.js
+++ src/textUtils.js
@@ -1,12 +1,21 @@
 export const capitalize = (str) => {
-  if (!str) return "";
+  if (!str) {
+    return "";
+  }
   return str[0].toUpperCase() + str.slice(1);
 };
 
 export const reverseString = (str) => {
+  if (!str) {
+    return "";
+  }
+
   return str.split("").reverse().join("");
 };
 
 export const countWords = (str) => {
+  if (!str) {
+    return 0;
+  }
   return str.trim() === "" ? 0 : str.trim().split(/\s+/).length;
 };
